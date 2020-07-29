ad32de5ea6c96e192d50d4406969301cf640d6c8
PWalis <pwilky1016@gmail.com>
2020-07-29 15:24:44 -0700

    Added location label to graph and links to github and linkedin

--- python/figure.py
+++ python/figure.py
@@ -8,11 +8,21 @@ class vis():
     
     def __init__(self, df):
         self.df = df
-    
+        self.location = list(df[['Region', 'Country', 'State', 'City']].iloc[0])
+
+    def loc_string(self, location):
+        country = location[1]
+        state = location[2]
+        city = location[3]
+        if state is not '' :
+            return f'{city}, {state}, {country}'
+        else:
+            return f'{city}, {country}'
+
     def create_vis(self):
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=self.df['Date'], y=self.df['AvgTemperature'], mode='markers', name='data', line={'color':'black'}))
-        fig.update_layout(plot_bgcolor='white', title='Historic Average Temperature',
+        fig.update_layout(plot_bgcolor='white',
                           annotations=[
                               dict(
                                   x=0.5,
@@ -32,6 +42,15 @@ class vis():
                                   font_size=20,
                                   xref='paper',
                                   yref='paper'
+                              ),
+                              dict(
+                                  x=0.03,
+                                  y=1.2,
+                                  showarrow=False,
+                                  text=self.loc_string(self.location),
+                                  font_size=20,
+                                  xref='paper',
+                                  yref='paper'
                               )
                           ],
                           autosize=False,
@@ -40,3 +59,4 @@ class vis():
                           )
                           )
         return fig
+
