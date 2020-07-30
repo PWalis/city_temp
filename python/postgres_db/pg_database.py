db5308693e93eae4281e61595707da9f9e2113b3
PWalis <pwilky1016@gmail.com>
2020-07-30 13:42:50 -0700

    that didn't work reverting to previous commit state

--- python/postgres_db/pg_database.py
+++ python/postgres_db/pg_database.py
@@ -49,7 +49,6 @@ class database():
             self.update_df(df)
             return vis(df).create_vis()
         else:
-            self.update_df(pd.DataFrame)
             return vis(pd.DataFrame).create_vis()
         
     def get_dict(self):
